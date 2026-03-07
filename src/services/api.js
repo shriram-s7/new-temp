a5b48ca2dcdc8e2177112e0aeb55a5f696c28aa9
Shriram <shriram0981234@gmail.com>
2026-03-08 03:21:25 +0530

    ec2 connect

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,28 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://ntqtyvyayk0.execute-api.us-east-1.amazonaws.com/prod";
+const API_BASE_URL =
+  "https://ntqtyvyayk0.execute-api.us-east-1.amazonaws.com/prod";
 
 export const predictBreast = async (formData) => {
-    return axios.post(`${API_BASE_URL}/predict/breast`, formData, {
-        headers: {
-        "Content-Type": "multipart/form-data",
-        },
-    });
-    };
+  return axios.post(`${API_BASE_URL}/predict/breast`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
-    export const predictCervical = async (formData) => {
-    return axios.post(`${API_BASE_URL}/predict/cervical`, formData, {
-        headers: {
-        "Content-Type": "multipart/form-data",
-        },
-    });
-    };
+export const predictCervical = async (formData) => {
+  return axios.post(`${API_BASE_URL}/predict/cervical`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
-    export const predictPCOS = async (formData) => {
-    return axios.post(`${API_BASE_URL}/predict/pcos`, formData, {
-        headers: {
-        "Content-Type": "multipart/form-data",
-        },
-    });
+export const predictPCOS = async (formData) => {
+  return axios.post(`${API_BASE_URL}/predict/pcos`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
